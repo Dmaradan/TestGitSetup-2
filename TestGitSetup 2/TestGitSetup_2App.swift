5635b3ed9c611b520bfa9a5b7cfc8a2b5a419718
Dmaradan <diegomartin1313@gmail.com>
2023-02-15 00:49:18 +0100

    Initial Commit

---
+++ TestGitSetup 2/TestGitSetup_2App.swift
@@ -0,0 +1,17 @@
+//
+//  TestGitSetup_2App.swift
+//  TestGitSetup 2
+//
+//  Created by Diego Martin on 2/15/23.
+//
+
+import SwiftUI
+
+@main
+struct TestGitSetup_2App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
